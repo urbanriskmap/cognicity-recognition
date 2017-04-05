eb0ad5e2d10c9145eeafbfed6b3173fe950cb477
Matthew Berryman <matthew@acrossthecloud.net>
2017-04-05 20:16:04 +1000

    lambda function
    1. Triggered from s3 event.
    2. Gives the object in s3 to AWS rekognition
    3. Takes the results and sends via AWS SNS

---
+++ index.js
@@ -0,0 +1,50 @@
+const util = require('util');
+const AWS = require('aws-sdk');
+const rekognition = new AWS.Rekognition();
+const sns = new AWS.SNS();
+
+exports.handler = (event, context, callback) => {
+    console.log("Reading input from event:\n", util.inspect(event, {depth: 5}));
+
+    let s3Record = event.Records[0].s3;
+    let srcBucket = s3Record.bucket.name;
+    let srcKey = decodeURIComponent(s3Record.object.key.replace(/\+/g, " "));
+
+    var params = {
+        Image: {
+            S3Object: {
+                Bucket: srcBucket,
+                Name: srcKey
+            }
+        },
+        MaxLabels: 10,
+        MinConfidence: 60
+    };
+
+    rekognition.detectLabels(params).promise().then(function(data) {
+        var message = process.env.BASE_URL +srcKey.replace("originals/","")+" , ";
+        for (var i = 0; i < data.Labels.length; i++) {
+          message += "Found " + data.Labels[i].Name + " with confidence: " + String(data.Labels[i].Confidence);
+          if (i < data.Labels.length-1) {
+            message += ", ";
+          }
+        }
+        sns.publish({
+                Message: message,
+                TopicArn: process.env.TOPIC_ARN;
+            },
+            function(err, data) {
+              if (err) {
+                console.log(err.stack);
+              }
+              console.log('sns sent');
+              console.log(data);
+            });
+      }
+    ).then(function (data) {
+        callback(null, data);
+    }).catch(function (err) {
+      callback(err);
+    });
+
+};
